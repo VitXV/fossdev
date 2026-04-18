09f1c03811f2983e47fada9b52cf2ff2c84aba9c
VitXV <thvitva@gmail.com>
2026-04-18 10:39:11 +0800

    test addiction with bug

--- testing/tests/test_math.py
+++ testing/tests/test_math.py
@@ -1,8 +1,11 @@
 import sys
 sys.path.append('../src')
-from math import add
+from math import (add, add_with_bug)
 def test_add():
     assert add(2,2) == 4
+    assert add_with_bug(0,0) == 0
+    assert add_with_bug(1,1) == 1
+    #assert add_with_bug(2,3) == 5
     print('test complited')
 if __name__ == '__main__':
     test_addiction()
