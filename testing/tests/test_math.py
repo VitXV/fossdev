8906d0873c3ecb9604d2f1c1b8bd76d6417f9db2
VitXV <thvitva@gmail.com>
2026-04-18 10:50:10 +0800

    added more tests

--- testing/tests/test_math.py
+++ testing/tests/test_math.py
@@ -1,11 +1,32 @@
 import sys
 sys.path.append('../src')
 from math import (add, add_with_bug)
+
 def test_add():
-    assert add(2,2) == 4
+    assert add(2,2) ==  4
+    print('test add')
+def test_with_bug():
     assert add_with_bug(0,0) == 0
     assert add_with_bug(1,1) == 1
     #assert add_with_bug(2,3) == 5
-    print('test complited')
+    print('test add_with_bug')
+def test_duplicated():
+    assert add(2,3) == 2+3
+    print('test duplicated')
+def test_reasonable():
+    assert add(2,2) == 4
+    assert add(0,0) == 0
+    assert add(4,5) == 9
+    assert add(-10,1) == -9
+    print('test reasonable')
+def test_commutative():
+    assert add(2,-2) == 0
+    assert add(-2,2) == 0
+    print('test commutative')
+
 if __name__ == '__main__':
-    test_addiction()
+    test_add()
+    test_with_bug()
+    test_duplicated()
+    test_reasonable()
+    test_commutative()
