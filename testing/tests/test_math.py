960630be243dba1cbea7c0035e5d37e240fa3a7e
VitXV <thvitva@gmail.com>
2026-04-18 11:01:05 +0800

    Added tax calculation and tests for it

--- testing/tests/test_math.py
+++ testing/tests/test_math.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../src')
-from math import (add, add_with_bug)
+from math import (add, add_with_bug, calculate_tax_with_bug, calculate_tax)
 
 def test_add():
     assert add(2,2) ==  4
@@ -24,9 +24,27 @@ def test_commutative():
     assert add(-2,2) == 0
     print('test commutative')
 
+def test_tax_calculation_with_bug():
+    try:
+        assert calculate_tax_with_bug(1000) == 150
+        assert calculate_tax_with_bug(100) == 15
+        assert calculate_tax_with_bug(10) == 1.5
+        assert calculate_tax_with_bug(1) == 0.15
+        assert calculate_tax_with_bug(0) == 0
+    except:
+        print("Tax Calculation failed")
+def test_tax_calculation():
+    assert calculate_tax(1000) == 150
+    assert calculate_tax(100) == 15
+    assert calculate_tax(10) == 1.5
+    assert calculate_tax(1) == 0.15
+    assert calculate_tax(13) == 1.95
+    print("Tax Calculation test complited")
 if __name__ == '__main__':
     test_add()
     test_with_bug()
     test_duplicated()
     test_reasonable()
     test_commutative()
+    test_tax_calculation()
+    test_tax_calculation_with_bug()
